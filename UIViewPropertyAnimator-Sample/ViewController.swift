752d556d73c1f74c088fc8fbb0bb91c94d8d5e7f
ddd503 <skbsporta7@gmail.com>
2019-03-07 22:15:56 +0900

    update animation

--- UIViewPropertyAnimator-Sample/ViewController.swift
+++ UIViewPropertyAnimator-Sample/ViewController.swift
@@ -8,13 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    @IBOutlet weak var animationView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
+    @IBAction func action(_ sender: UIButton) {
 
+    }
+
+    func 単純な横移動() {
+        let endPointX = animationView.center.x + 50
+        let animator = UIViewPropertyAnimator(duration: 1.0, curve: .easeIn) {
+            self.animationView.center.x = endPointX
+        }
+        animator.startAnimation()
+    }
 }
-
